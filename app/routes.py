40b02d31a484c63d25b58261c185da8dfeed1760
wanjunlan <wanjunlan@gmail.com>
2022-10-20 15:52:26 -0700

    added planet class and made a planet endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,29 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+class Planet():
+    def __init__(self, id, name, description):
+        self.id = id
+        self.name = name
+        self.description = description
+
+
+planet_items = [
+    Planet(1, "Mercury", "the smallest planet"),
+    Planet(2, "Venus", "the hottest planet"),
+    Planet(3, "Earth", "the best planet"),
+    Planet(4, "Mars", "the red planet"),
+    Planet(5, "Jupiter", "the biggest planet"),
+    Planet(6, "Saturn", "the most rings planet"),
+    Planet(7, "Uranus", "named after the Greek god of the sky"),
+    Planet(8, "Neptune", "the last planet")
+]
+
+planet_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@planet_bp.route("", methods=["GET"])
+def get_all_planet():
+    result = []
+    for item in planet_items:
+        item_dict = {"id": item.id, "name": item.name, "description": item.description}
+        result.append(item_dict)
+    return jsonify(result), 200
